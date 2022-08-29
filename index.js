8a7596fc752d382f4bf091b593cdf19cf48c5f5e
chris <chrishethertonweb@gmail.com>
2022-08-29 12:15:10 +0200

    classes created

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 class CoffeeShop {
-	constructor (name, menu, orders) {
-		
+
+	constructor(name) {
+
+		this.name = name
+		this.menuItem = []
+		this.orders = []
+
+	}
+}
+
+class MenuItem {
+
+	constructor(name, type, price) {
+
+		this.name = name
+		this.type = type
+		this.price = price
+
+	}
+}
+
+class Order {
+	constructor(name) {
+
+		this.name = name
+
+	}
+
+}
+
+
+class MenuItemManager {
+
+	constructor(menuItem) {
+
+		this.menuItem = menuItem
+	}
+
+}
+
+
+
+class OrdersManager {
+
+	constructor() {
+
 	}
 }
